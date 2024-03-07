4f7362eaadec27fb8a241e16ef1b7479a7098c26
Omar Darazi <folk21434@gmail.com>
2024-03-07 10:35:39 +0200

    first

---
+++ lib/register/view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'logic.dart';
+
+class RegisterPage extends StatelessWidget {
+  RegisterPage({Key? key}) : super(key: key);
+
+  final logic = Get.put(RegisterLogic());
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
